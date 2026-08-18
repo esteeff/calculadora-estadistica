d8a857cada0115c96e531399f314b20eb1d3b6fa
esteeeff <fani35546@gmail.com>
2026-08-18 11:20:25 -0700

    Agregar calculo de media

---
+++ src/CalculadoraEstadistica.java
@@ -0,0 +1,21 @@
+public class CalculadoraEstadistica {
+
+    public static double calcularMedia(double datos) {
+        double suma = 0;
+        
+        for(double dato : datos){
+            suma += dato;
+        }
+        
+        return suma / datos.length;
+        
+    }
+    
+    public static void main(String args) {
+        double datos = {10, 20, 30, 40, 50};
+        
+        Stystem.out.println("Media =" + calculadoraMedia(datos));
+    }
+}
+        
+        
