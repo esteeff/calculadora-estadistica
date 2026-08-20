7721ec681838f28f234c3d4e9ee6b2f09fc83c21
esteeeff <fani35546@gmail.com>
2026-08-20 16:41:53 -0700

    Correccion en formato de metodo main

--- src/CalculadoraEstadistica.java
+++ src/CalculadoraEstadistica.java
@@ -1,42 +1,13 @@
 public class CalculadoraEstadistica {
-
-    public static double calcularMedia(double datos) {
+    public static double calcularMedia(double[] datos) {
         double suma = 0;
-        
-        for(double dato : datos){
+        for (double dato : datos) {
             suma += dato;
         }
-        
         return suma / datos.length;
-        
     }
-    
-    public static void main(String args) {
-        double datos = {10, 20, 30, 40, 50};
-        
-        Stystem.out.printf("Media = %.2f%n", calculadorMedia(datos));
+    public static void main(String[] args) {
+        double[] datos = {10, 20, 30, 40, 50};
+        System.out.println("Media = " + calcularMedia(datos));
     }
-    
-    
-    public static double calcularDesviacionEstandar(double datos) {
-        double media = calcularMedia(datos);
-        double suma = 0;
-        
-        for (double dato : datos) {
-            suma += Math.pow(dato - media, 2) ;
-        }
-        
-        
-        return Math.sqrt(suma / datos.length);
-    
-    }
-    
-    System.out.printf(
-        "Desviacion estandar = %.2f%n",
-        calcularDesviacionEstandar(Datos)
-    );
-    
 }
-
-        
-        
